6dbc6ac0eacb1d5120798a716eb75cbd46ec84fb
LuismGil <luism6977@gmail.com>
2024-08-28 22:58:19 -0300

    feat: update Header

--- src/components/header/headerTracker.vue
+++ src/components/header/headerTracker.vue
@@ -1,29 +1,58 @@
 <template>
-  <nav>
-    <div>
-      <div>
-        <q-btn
-          v-for="(button, index) in buttons"
-          :key="index"
-          :disabled="disabled"
-          flat
-          round
-          @click="button.click"
-          :style="{ color: button.iconColor }"
-        >
-          <q-icon
-            :name="button.icon"
-          />
-          <Tooltip v-if="button.tooltip" :content="button.tooltip" />
-        </q-btn>
-        <q-toggle
-          v-model="isDark"
-          label="Modo Oscuro"
-          icon="fa-solid fa-sun"
-          checked-icon="fa-solid fa-moon"
-          color="black"
-        />
-      </div>
+  <nav class="flex justify-end p-4 bg-gray-100 dark:bg-gray-800">
+    <div class="flex items-center space-x-4">
+      <q-btn
+        flat
+        round
+        icon="fa-solid fa-globe"
+        @click="toggleMenu"
+      />
+      <q-menu
+        v-model="menu"
+        transition-show="scale"
+        transition-hide="scale"
+        cover
+        class="q-mr-sm"
+      >
+        <q-list>
+          <q-item clickable @click="setLanguage('en')">
+            <q-item-section>
+              English
+            </q-item-section>
+          </q-item>
+          <q-item clickable @click="setLanguage('es')">
+            <q-item-section>
+              Español
+            </q-item-section>
+          </q-item>
+          <q-item clickable @click="setLanguage('pt')">
+            <q-item-section>
+              Português
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-menu>
+      <q-toggle
+        v-model="isDark"
+        icon="fa-solid fa-sun"
+        checked-icon="fa-solid fa-moon"
+        class="q-mr-sm"
+      >
+        <Tooltip :content="isDark ? 'Modo Claro' : 'Modo Oscuro'" />
+      </q-toggle>
+      <q-btn
+        v-for="(button, index) in buttons"
+        :key="index"
+        :disabled="disabled"
+        flat
+        round
+        @click="button.click"
+        :style="{ color: button.iconColor }"
+        class="flex items-center"
+      >
+        <q-icon :name="button.icon" />
+        <Tooltip v-if="button.tooltip" :content="button.tooltip" />
+      </q-btn>
     </div>
   </nav>
 </template>
@@ -39,6 +68,7 @@ const props = defineProps<HeaderProps>()
 const $q = useQuasar()
 
 const isDark = ref($q.dark.isActive)
+const menu = ref<boolean>(false)
 
 const buttons = ref([
   {
@@ -58,6 +88,41 @@ const buttons = ref([
     tooltip: 'Send mail'
   }
 ])
+
+const languages = ref([
+  {
+    language: 'en',
+    name: 'English',
+    icon: '',
+    click: () => {
+      setLanguage('en')
+    }
+  },
+  {
+    language: 'es',
+    name: 'Spanish',
+    icon: '',
+    click: () => {
+      setLanguage('es')
+    }
+  },
+  {
+    language: 'pt-bt',
+    name: 'Portuguese',
+    icon: '',
+    click: () => {
+      setLanguage('en')
+    }
+  }
+])
+
+const toggleMenu = () => {
+  menu.value = !menu.value
+}
+
+const setLanguage = (language: string) => {
+  console.log(`Idioma seleccionado: ${language}`)
+}
 
 watch(isDark, (newValue) => {
   $q.dark.set(newValue)
